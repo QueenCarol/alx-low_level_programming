5f84de3c6c6f99271678bc7736e82e85398d776d
“QueenCarol” <carolgacheru6@gmail.com>
2022-09-12 12:39:44 -0700

    Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,12 @@
-
 #include <stdio.h>
 
 /**
-* main - prints the string in the put function
-*
-* Description: using the main function 
-* this program prints "Programming is like building a multilingual puzzle
-* Return: Always 0 (Success)
-*/
-
+ *  * main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
 int main(void)
 {
-       	puts("\"Programming is like building a multilingual puzzle");
-       	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
