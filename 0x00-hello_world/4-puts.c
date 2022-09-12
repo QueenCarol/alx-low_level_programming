a89635c81990fab60495215b1730bcccd87b4c24
“QueenCarol” <carolgacheru6@gmail.com>
2022-09-12 06:38:23 -0700

    Prints as exact

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,14 @@
-#!/bin/bash
+
 #include <stdio.h>
 
-int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+       	puts("/" "Programming is like building a multilingual puzzle");
+       	return (0);
+}
