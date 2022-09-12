be6d01f3fa5c6e6d0433b73b3e61b404460b9e7c
“QueenCarol” <carolgacheru6@gmail.com>
2022-09-12 11:30:38 -0700

    Printing Betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,8 @@
 */
 
 int main(void)
+
 {
-       	puts("/" "Programming is like building a multilingual puzzle");
+       	puts("Programming is like building a multilingual puzzle\n");
        	return (0);
 }
