24a2a4e03707c9e96bad239f25998664fd71b7ab
“QueenCarol” <carolgacheru6@gmail.com>
2022-09-15 02:13:48 -0700

    9 times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,7 +2,6 @@
 
 /**
  * times_table - function that  prints the 9 times table, starting with 0
- * Return: nill
  */
 void times_table(void)
 {
@@ -12,7 +11,7 @@ void times_table(void)
 	{
 		_putchar('0');
 
-		for (mult = 1; mult<= 9; mult++);
+		for (mult = 1; mult<= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
@@ -22,8 +21,9 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) + 10);
-			_putchar((prod % 10) + 10);
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) +  '0');
 		}
 		_putchar('\n');
 	}
