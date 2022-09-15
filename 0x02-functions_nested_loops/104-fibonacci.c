cb1b49a39b8cc46fc1b336c865afec86c3824fd9
“QueenCarol” <carolgacheru6@gmail.com>
2022-09-15 15:26:50 -0700

    98 num

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - program that finds and prints 
+ * main - program that finds and prints
  * the first 98 Fibonacci numbers
  * starting with 1 and 2, followed by a new line.
  * Return: always 0
@@ -9,19 +9,22 @@
 int main(void)
 {
 	int num;
-	unsigned long n1 = 0, n2 = 1, n3;
+	unsigned long t0 = 0, t1 = 1, s;
 
-	for (num = 0; num < 98; num++)
+	for (num = 1; num <= 98; num++)
 	{
-		n3 = n1 + n2;
-		printf("%lu", n3);
-		n1 = n2;
-		n2 = n3;
+		s = t0 + t1;
 
-		if (num == 97)
-			printf("\n");
+		if (num != 98)
+		{
+			printf("%lu, ", s);
+		}
 		else
-			printf(", ");
+		{
+			printf("%lu\n", s);
+		}
+	t0 = t1;
+	t1 = s;
 	}
 	return (0);
 }
