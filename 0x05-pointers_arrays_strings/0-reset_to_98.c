4ed6d2b1eab980cf25c3eaeda0fea0fe98d182d7
“QueenCarol” <carolgacheru6@gmail.com>
2022-09-19 23:29:05 -0700

    resets the value of an integer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,6 +3,7 @@
 /**
  * reset_to_98 -  that takes a pointer to an int as parameter
  * and updates the value it points to to 98.
+ * @n: is the int used to point the value
  * Return: always 0
  */
 
