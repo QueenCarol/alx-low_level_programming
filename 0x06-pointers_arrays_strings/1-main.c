b4e3dfb2b6c09374ded9fffe3267684c6d889e83
“QueenCarol” <carolgacheru6@gmail.com>
2022-09-23 00:20:36 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/1-main.c
+++ 0x06-pointers_arrays_strings/1-main.c
@@ -1,11 +1,11 @@
-i#include "main.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	    char s1[98] = "Hello ";
