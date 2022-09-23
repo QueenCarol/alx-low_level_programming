47d692faa7f84209ec67b10e6410a8555c8c9add
“QueenCarol” <carolgacheru6@gmail.com>
2022-09-23 02:01:27 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,7 +4,7 @@
  * _strcat - function that concatenates two strings
  * @scr: string source
  * @dest: destination of string
- * Return:  pointer to the resulting string dest
+ * Return: pointer to the resulting string @dest
  */
 
 char *_strcat(char *dest, char *src)
